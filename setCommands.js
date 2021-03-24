fa6dbbaae6d971d96cd45cf9a7b5201747ad7033
Dan Jakob Ofer <dan@ofer.to>
2021-03-24 14:18:33 -0400

    Get the current tab from onCommand

--- setCommands.js
+++ setCommands.js
@@ -1,34 +1,31 @@
 (function() {
-  chrome.commands.onCommand.addListener(function(command) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      var currentTab = tabs[0];
-      var tabUrl = new URL(currentTab.url);
+  chrome.commands.onCommand.addListener(function(command, tab) {
+    var tabUrl = new URL(tab.url);
 
-      if (tabUrl.origin === "https://teams.microsoft.com" && tabUrl.hash.match(/(meeting|calling)/)) {
-        if (command === "open-chat") {
-          chrome.tabs.executeScript({
-            code: "window.document.querySelector('#chat-button').click();"
-          });
-        }
-
-        if (command === "open-roster") {
-          chrome.tabs.executeScript({
-            code: "window.document.querySelector('#roster-button').click();"
-          });
-        }
-
-        if (command === "open-video") {
-          chrome.tabs.executeScript({
-            code: "window.document.querySelector('#video-button').click();"
-          });
-        }
-
-        if (command === "open-share") {
-          chrome.tabs.executeScript({
-            code: "window.document.querySelector('#share-button').click();"
-          });
-        }
+    if (tabUrl.origin === "https://teams.microsoft.com" && tabUrl.hash.match(/(meeting|calling)/)) {
+      if (command === "open-chat") {
+        chrome.tabs.executeScript({
+          code: "window.document.querySelector('#chat-button').click();"
+        });
       }
-    });
+
+      if (command === "open-roster") {
+        chrome.tabs.executeScript({
+          code: "window.document.querySelector('#roster-button').click();"
+        });
+      }
+
+      if (command === "open-video") {
+        chrome.tabs.executeScript({
+          code: "window.document.querySelector('#video-button').click();"
+        });
+      }
+
+      if (command === "open-share") {
+        chrome.tabs.executeScript({
+          code: "window.document.querySelector('#share-button').click();"
+        });
+      }
+    }
   });
 })();
