fd8d40723c0ba7df84bf12ce6100b9d6ff736d02
Dan Jakob Ofer <dan@ofer.to>
2023-02-17 06:29:40 -0500

    chore(outlook): Bavardo is now for MS Outlook

--- setCommands.js
+++ setCommands.js
@@ -2,28 +2,10 @@
   chrome.commands.onCommand.addListener(function(command, tab) {
     var tabUrl = new URL(tab.url);
 
-    if (tabUrl.origin === "https://teams.microsoft.com" && tabUrl.hash.match(/(meeting|calling)/)) {
-      if (command === "open-chat") {
+    if (tabUrl.origin === "https://outlook.office365.com") {
+      if (command === "open-categories") {
         chrome.tabs.executeScript({
-          code: "window.document.querySelector('#chat-button').click();"
-        });
-      }
-
-      if (command === "open-roster") {
-        chrome.tabs.executeScript({
-          code: "window.document.querySelector('#roster-button').click();"
-        });
-      }
-
-      if (command === "open-video") {
-        chrome.tabs.executeScript({
-          code: "window.document.querySelector('#video-button').click();"
-        });
-      }
-
-      if (command === "open-share") {
-        chrome.tabs.executeScript({
-          code: "window.document.querySelector('#share-button').click();"
+          code: "document.getElementById(\"509\").click()"
         });
       }
     }
