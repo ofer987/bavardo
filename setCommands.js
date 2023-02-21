a609db0ab20018dcec2e3a37a48bac6a1ef71bfc
Dan Jakob Ofer <dan@ofer.to>
2023-02-21 07:48:33 -0500

    can be used in standalone Outlook Chrome App

--- setCommands.js
+++ setCommands.js
@@ -2,7 +2,7 @@
   chrome.commands.onCommand.addListener(function(command, tab) {
     var tabUrl = new URL(tab.url);
 
-    if (tabUrl.origin === "https://outlook.office365.com") {
+    if (tabUrl.origin === "https://outlook.office365.com" || tabUrl.origin === "https://outlook.office.com") {
       if (command === "open-categories") {
         chrome.tabs.executeScript({
           code: "document.getElementById(\"509\").click()"
